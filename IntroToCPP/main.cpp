a299f255065a59653290a560bb32c488a9444f0c
Ashmeet Kaur <ashmeet-kaur1@myseneca.ca>
2024-01-17 21:38:15 -0500

    First file

---
+++ IntroToCPP/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cout << "Hello my name is Ashmeet Kaur" << endl;
+    cout << "enter your age:";
+    cin >> age;
+    cout << "Your age is:" << age << endl;
+    cout<<"Thank you for input"<<endl;
+    return 0;
+}
